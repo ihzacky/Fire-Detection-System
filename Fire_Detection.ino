53957cdb1c9f26c7e607a49ca13e0f9c38685fee
ihzacky <114144242+ihzacky@users.noreply.github.com>
2024-12-21 01:23:51 +0700

    fix blynk connection and logic problems

--- Fire_Detection.ino
+++ Fire_Detection.ino
@@ -1,3 +1,9 @@
+// Wifi & Blynk Credentials are located in credentials.h
+#define BLYNK_TEMPLATE_ID CRED_BLYNK_T_ID
+#define BLYNK_TEMPLATE_NAME CRED_BLYNK_T_NAME
+#define BLYNK_AUTH_TOKEN CRED_BLYNK_AUTH_TOKEN
+#define BLYNK_PRINT Serial
+
 #include "credentials.h"
 #include "pins_arduino.h"
 
@@ -9,25 +15,28 @@
 
 #include <string.h>
 
-// Wifi & Blynk Credentials are located at credentials.h
-
 // Define PIN
-#define FLAME_SENSOR_PIN D6
-#define BUZZER_PIN D5
-
-#define LED_RED_PIN D3
-#define LED_GREEN_PIN D4
-
-#define DHT_PIN D8
+//INPUT
+#define FLAME_SENSOR_PIN D11
+#define DHT_PIN D12
 #define DHTTYPE DHT11
 
-#define SERVO_PIN D7
-#define RELAY_PIN D9
+//OUTPUT
+#define BUZZER_PIN D4
+#define LED_RED_PIN D8
+#define LED_GREEN_PIN D9
+
+//ACTUATOR-OUTPUT
+#define SERVO_PIN D4 // need pwm
+#define RELAY_PIN D3
 
 // Variables
 DHT dht(DHT_PIN, DHTTYPE);
 Servo servo;
 BlynkTimer timer;
+
+const char WIFI_SSID[] = CRED_WIFI_SSID;
+const char WIFI_PASS[] = CRED_WIFI_PASS;
 
 int flameState = 0;
 float temperature = 0.0;
@@ -48,27 +57,35 @@ void sendDHTData() {
   if (!isnan(temperature) && !isnan(humidity)) {
     Blynk.virtualWrite(V1, temperature); // Send Temperature
     Blynk.virtualWrite(V2, humidity);    // Send Humidity
-  } else {
-    Serial.println(F("## temp/humidity null"));
-
-  }
+  } 
 }
 
 // Servo Control
 BLYNK_WRITE(V3) {
-  int position = param.asInt();
+  unsigned short position = param.asInt();
   servo.write(position);
 }
 
 // Relay control
-void pumpControl(char *command){
+void pumpControl(const char *command){
   if (strcmp(command, "ON") == 0) {
-    digitalWrite(RELAY_PIN, HIGH);
-
-  } else if (strcmp(command, "OFF") == 0) {
     digitalWrite(RELAY_PIN, LOW);
 
+  } else if (strcmp(command, "OFF") == 0) {
+    digitalWrite(RELAY_PIN, HIGH);
+
   }
+}
+
+
+void buzzerControl(){
+
+
+}
+
+// override test
+BLYNK_WRITE(V9){
+  flameState = param.asInt();
 }
 
 // Logic
@@ -94,23 +111,53 @@ void mainLogic(){
     Blynk.virtualWrite(V4, 0); // Buzzer OFF
 
   }
+  
 }
 
-void checkWifi(){
-  Serial.print("Wifi Status: ");
+void wifiConnect(){
+  Serial.println("Connecting to WiFi!");
+  WiFi.begin(WIFI_SSID, WIFI_PASS); 
+
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+
+  }
+  Serial.println();
+  wifiCheck();
+
+}
+
+void wifiCheck(){
   if(WiFi.status() == WL_CONNECTED){
-    Serial.println("Connected!");
-    Serial.print("IP address: ");
+    Serial.println(F("Connected"));
+    Serial.print(F("IP address: "));
     Serial.println(WiFi.localIP());
 
   } else{
-    Serial.println("Disconnected!");
+    Serial.println(F("Disconnected"));
+    wifiConnect();
+
+  }
+}
+
+void connectionCheck(){
+  if(!(Blynk.connected())){
+    Serial.println(F("Disconnected"));
+    Serial.println(F("Trying to connect to Blynk Server"));
+   
+    while (Blynk.connect(3333) == false) {
+      Serial.print(F("."));
+
+    }
+  } else{
+    Serial.println(F("Connected"));  
 
   }
 }
 
 void logAll(){
-  Serial.println(F("+-----------------------+"));
+  Serial.println(F("\n+-----------------------+"));
   Serial.println(F("| Log all functionality |"));
   Serial.println(F("+-----------------------+"));
 
@@ -123,14 +170,12 @@ void logAll(){
   Serial.print(F("Humid: "));
   Serial.println(humidity);
   
-  checkWifi();
+  Serial.print(F("Wifi Status: "));
+  wifiCheck();
 
-  Serial.print(F("Blynk: "));
-  if(Blynk.connected()){
-    Serial.println(F("Connected"));
-  } else{
-    Serial.println(F("Disconnected"));
-  }
+  Serial.print(F("Blynk Status: "));
+  connectionCheck();
+
 }
 
 void setup() {
@@ -138,6 +183,7 @@ void setup() {
 
   // INPUT
   pinMode(FLAME_SENSOR_PIN, INPUT);
+  pinMode(DHT_PIN, INPUT);
   dht.begin();
 
   // OUTPUT
@@ -145,26 +191,29 @@ void setup() {
   pinMode(LED_RED_PIN, OUTPUT);
   pinMode(LED_GREEN_PIN, OUTPUT);
   pinMode(RELAY_PIN, OUTPUT);
-
   pinMode(BUILTIN_LED, OUTPUT);
 
   servo.attach(SERVO_PIN);
 
-  // Connect to blynk
-  Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASS);
-  checkWifi();
+  //Connect to wifi
+  wifiConnect();
+
+  //Connect to blynk
+  Blynk.config(BLYNK_AUTH_TOKEN); 
+  connectionCheck();
 
   // Timers for sending data
-  timer.setInterval(1000L, sendFlameData); // Flame Sensor every 1 sec
-  timer.setInterval(2000L, sendDHTData);   // DHT Data every 2 sec
+  timer.setInterval(3000L, sendFlameData);
+  // timer.setInterval(3000L, sendDHTData);   
+  timer.setInterval(5000L, logAll); // Print all sensor and functionalities status to serial every 5 sec
 
-  timer.setTimeout(5000L, logAll);
+  // run main logic
+  timer.setInterval(1500L, mainLogic);
 
 }
 
 void loop() {
   Blynk.run();
   timer.run();
-
-  mainLogic(); 
+  
 }
